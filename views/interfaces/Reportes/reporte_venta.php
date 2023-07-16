96bf353270d0909a75af6a9ca45408ab2a120615
Sierra <sasierra462@soy.sena.edu.co>
2023-07-16 17:12:41 -0500

    se añadio diseño a los reportes

--- views/interfaces/Reportes/reporte_venta.php
+++ views/interfaces/Reportes/reporte_venta.php
@@ -1,35 +1,77 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "PiedraSportsDB";
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        div{
+            margin: 150px auto;
+            width: 600px;
+        }
+        table {
+            background-color: #ffffff;
+            text-align: left;
+            border-collapse: collapse;
+            width: 100%;
+        }
+        
+        th, td {
+            border: 1px solid black;
+            padding: 8px;
+        }
+        
+        thead {
+            background-color: #143d59;
+            border-bottom: solid 5px #121e4a;
+            color: #ffffff;
+        }
+        
+        tr:nth-child(even) {
+            background-color: #dddddd;
+        }
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
+        tr:over td{
+            background-color: #12394a;
+            color: #ffffff;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "PiedraSportsDB";
 
-$sql = "SELECT CANT_PRODUCTO_VENTA, FECHA_VENTA, PRECIO_TOTAL_VENTA FROM VENTA";
-$result = $conn->query($sql);
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        if ($conn->connect_error) {
+            die("Conexión fallida: " . $conn->connect_error);
+        }
 
-if ($result->num_rows > 0) {
-    echo "<table>
-            <tr>
-                <th>Cantidad de Producto</th>
-                <th>Fecha</th>
-                <th>Precio Total</th>
-            </tr>";
-    while ($row = $result->fetch_assoc()) {
-        echo "<tr>
-                <td>" . $row["CANT_PRODUCTO_VENTA"] . "</td>
-                <td>" . $row["FECHA_VENTA"] . "</td>
-                <td>" . $row["PRECIO_TOTAL_VENTA"] . "</td>
-            </tr>";
-    }
-    echo "</table>";
-} else {
-    echo "No se encontraron resultados.";
-}
+        $sql = "SELECT CANT_PRODUCTO_VENTA, FECHA_VENTA, PRECIO_TOTAL_VENTA FROM VENTA";
+        $result = $conn->query($sql);
 
-$conn->close();
-?>
+        if ($result->num_rows > 0) {
+            echo "<div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Cantidad de Producto</th>
+                                <th>Fecha</th>
+                                <th>Precio Total</th>
+                            </tr>
+                        </thead>";
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>
+                        <td>" . $row["CANT_PRODUCTO_VENTA"] . "</td>
+                        <td>" . $row["FECHA_VENTA"] . "</td>
+                        <td>" . $row["PRECIO_TOTAL_VENTA"] . "</td>
+                    </tr>";
+            }
+            echo "</table> </div>";
+        } else {
+            echo "No se encontraron resultados.";
+        }
+
+        $conn->close();
+    ?>
+</body>
+</html>
